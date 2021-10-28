4fa2d06af5f34261a1d92ba0615c6b46b33f8150
ivankaz763 <41322467+ivankaz763@users.noreply.github.com>
2021-10-28 16:54:57 +0300

    Update task2.rb

--- ruby/task2.rb
+++ ruby/task2.rb
@@ -16,16 +16,10 @@ end
 
 # рекурсия с хвостовой оптимизацией
 def tail_call_factorial(num, accumulator = 1)
-    if num < 0 
-        puts "negativ argument"
-    elsif num == 0
-        accumulator
-    else 
-        tail_call_factorial(num - 1, accumulator * num)
-    end
-end 
+    puts "negativ argument" if num < 0
+    return accumulator if num == 0
+    return tail_call_factorial(num - 1, accumulator * num)
+  end
 
-
-# puts factorial(1000).to_s.size
 puts factorial(10000).to_s.size
 puts tail_call_factorial(100000).to_s.size
