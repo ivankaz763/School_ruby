fc5dfc87aee8a7e4062c71a037aa3f0d4438e6b1
ivankaz763 <41322467+ivankaz763@users.noreply.github.com>
2021-12-20 19:13:09 +0300

    Update task_1.rb

--- api/task_1.rb
+++ api/task_1.rb
@@ -10,8 +10,10 @@ module VK_api_client
         def initialize(access_token = nil, api_version = '5.131', timeout = 60)
             @access_token = access_token
             @api_version = api_version
+            @timeout = timeout
         end
   
+        private
         def method_missing(api_method, *api_params)
             api_method_for_request = api_method.to_s.split('_').join('.')
             api_response = request_creator(api_method_for_request, *api_params)
@@ -19,7 +21,6 @@ module VK_api_client
             api_response['response']
         end
 
-        private
         def request_creator(api_method_for_request, params = {})
             params.merge!(access_token: @access_token,  v: @api_version, https: '1')
             url = "https://api.vk.com/method/#{api_method_for_request}"
@@ -32,7 +33,7 @@ module VK_api_client
             use_ssl = uri.scheme == 'https'
             request = Net::HTTP::Post.new(uri)
             request.form_data = params
-            Net::HTTP.start(uri.hostname, uri.port, use_ssl: use_ssl, read_timeout: timeout, open_timeout: timeout) { |http| http.request(request) }
+            Net::HTTP.start(uri.hostname, uri.port, use_ssl: use_ssl, read_timeout: @timeout, open_timeout: @timeout) { |http| http.request(request) }
         end
 
         class Error_api < StandardError
@@ -56,4 +57,4 @@ api = VK_api_client::API.new('6df71ec91de1b546e9d8e5189cc05d84ce38f0d3cb4dad57d0
 # возвращает информацию о пользователе
 puts api.users_get(user_ids: '210700286')
 # возвращает ошибка потому, что для вызова метода "getFollowers" нужна авторизация не по сообществу, а по странице
-puts api.users_getFollowers(user_ids: '210700286')
+api.users_gasdsetFollowers(user_ids: '210700286')
